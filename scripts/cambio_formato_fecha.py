64e239f838fd5e32b5d7c20408e6853ca7504349
AndresEspinal <ajulian.espinal@udea.edu.co>
2024-07-17 21:59:32 -0500

    Inicia proceso de clusterización

--- scripts/cambio_formato_fecha.py
+++ scripts/cambio_formato_fecha.py
@@ -8,9 +8,9 @@ Cambiar a formato de fecha
 
 """
 
-class DateFormat ():
+class DateFormat:
 
-    def __init__(self, columna, dataframe, porcentaje, profit, bet, co, result):
+    def __init__(self, columna, dataframe, porcentaje, profit, bet, co, result, dropid, dropgameid):
         self.date_columna=columna
         self.dataframe=dataframe
         self.porc_columna=porcentaje
@@ -18,6 +18,9 @@ class DateFormat ():
         self.bet_columna=bet
         self.co_columna=co
         self.result_columna=result
+        self.drop_id=dropid
+        self.drop_gameid=dropgameid
+
 
     def fecha(self):
         self.dataframe[self.date_columna]=self.dataframe[self.date_columna].apply(lambda x: datetime.fromisoformat(x.replace('Z','+00:00')))
@@ -37,11 +40,15 @@ class DateFormat ():
         self.dataframe[self.result_columna] = np.where(self.dataframe[self.prof_columna] > 0, 1, 0)
         return self.dataframe
     
+    def dropcolumns(self):
+        self.dataframe=self.dataframe.drop(columns=[self.drop_id, self.drop_gameid], axis=1)
+    
     def ejec_dateformat(self):
         self.fecha()
         self.porcentaje()
         self.imputacion()
         self.new_column()
+        self.dropcolumns()
         return self.dataframe
         
 
